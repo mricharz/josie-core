c312b13321c9c6840d5c0ca299346a2f02e2f170
mricharz_ext <manuel.richarz@holidaycheck.com>
2014-11-18 17:47:58 +0100

    Also allow Namespaces-Objects as HTML-Property

--- src/js/com/nysoft/josie/core/ManagedObject.js
+++ src/js/com/nysoft/josie/core/ManagedObject.js
@@ -42,7 +42,8 @@ com.nysoft.josie.core.EventStack.bind('com.nysoft.josie.core.ManagedObject', 'on
                 }
 			} catch (err) { //otherwise look for binding selector
 				Josie.log.trace('Parsed as string-value');
-				value = sValue;
+                //check if it is a namespace or object
+				value = Josie.getClass(sValue) || sValue;
 			}
 			options[propertyName] = value;
 		});
