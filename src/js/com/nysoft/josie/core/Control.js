c8faa9625b2f850fff69340085b8a74996a5c491
mricharz_ext <manuel.richarz@holidaycheck.com>
2014-11-20 15:33:44 +0100

    Added content as property to core.Control Added renderContent-Method to core.Control Added renderContentItem-Method to core.Control

--- src/js/com/nysoft/josie/core/Control.js
+++ src/js/com/nysoft/josie/core/Control.js
@@ -2,92 +2,102 @@ Josie.require('com.nysoft.josie.core.ManagedObject');
 
 //onBeforeInit
 com.nysoft.josie.core.EventStack.bind('com.nysoft.josie.core.Control', 'onBeforeInit', function(e) {
-	var oControlObject = e[0];
-	// set default id
-	(!oControlObject.getId())
-		&& oControlObject.setId(Josie.utils.uniqueId());
+    var oControlObject = e[0];
+    // set default id
+    (!oControlObject.getId())
+    && oControlObject.setId(Josie.utils.uniqueId());
 });
 
 //onAfterInit
 com.nysoft.josie.core.EventStack.bind('com.nysoft.josie.core.Control', 'onAfterInit', function(e) {
-	var oControlObject = e[0], arguments = e[1], domObject, options;
-	domObject = arguments[0] || null;
-	options = arguments[1] || null;
-	
-	//render control
-	oControlObject.trigger('onBeforeRenderer');
-	oControlObject._renderControl();
+    var oControlObject = e[0], arguments = e[1], domObject, options;
+    domObject = arguments[0] || null;
+    options = arguments[1] || null;
 
-	//update all properties to force rendering
-	oControlObject._forceUpdateProperties();
-	oControlObject.trigger('onAfterRenderer');
-	
+    //render control
+    oControlObject.trigger('onBeforeRenderer');
+    oControlObject._renderControl();
+
+    //update all properties to force rendering
+    oControlObject._forceUpdateProperties();
+    oControlObject.trigger('onAfterRenderer');
+
 });
 
 com.nysoft.josie.core.ManagedObject.extend('com.nysoft.josie.core.Control', {
-	
-	meta: {
-		id: 'string',
-		visible: 'boolean',
-        cssClasses: { type: 'object', defaultValue: [] }
-	},
-	
-	init: function() {},
-	
-	_renderControl: function() {
-		if(this.getDom()) {
-			//set id to dom-Element
-			this.getDom().attr('id', this.getId());
-			//is visible?
-			if(this.getVisible() === false) {
-				this.getDom().hide();
-			}
-            //bind global control events
-            this.getDom().click(jQuery.proxy(function(){
-                this.trigger('onClick');
-            }, this));
-            this.getDom().dblclick(jQuery.proxy(function(){
-                this.trigger('onDblClick');
-            }, this));
-            this.getDom().focus(jQuery.proxy(function(){
-                this.trigger('onFocus');
-            }, this));
-            this.getDom().blur(jQuery.proxy(function(){
-                this.trigger('onBlur');
-            }, this));
-		}
-	},
-	
-	_forceUpdateProperties: function() {
-		var aProperties = this.getProperties();
-		jQuery.each(aProperties, jQuery.proxy(function(key, value) {
-			var setter = 'set'+Josie.utils.capitalize(key.slice(1));
-			if(this[setter]) {
-				Josie.log.trace('Call '+setter, value);
-				this[setter].call(this, value);
-			}
-		}, this));
-	},
-	
-	rerender: function(bWithoutUpdateProperties) {
-		this.trigger('onBeforeRenderer');
-		this.replaceDom('<div />').remove();
-		this._renderControl();
-		if(!bWithoutUpdateProperties)
-			this._forceUpdateProperties();
-		this.trigger('onAfterRenderer');
-	},
-	
-	invalidate: function() {
-		Josie.log.debug('Invalidating '+this.className);
-		if(this.__valid !== false) {
-	  		this.__valid = false;
-			setTimeout(jQuery.proxy(function() {
-	   			this.__valid = true;
-				this.rerender();
-			}, this), 0);
-		}
-	},
+
+    meta: {
+        id: 'string',
+        visible: 'boolean',
+        cssClasses: { type: 'object', defaultValue: [] },
+        content: 'object'
+    },
+
+    init: function() {},
+
+    _renderControl: function() {
+        if(this.getDom()) {
+            //set id to dom-Element
+            this.getDom().attr('id', this.getId());
+            //is visible?
+            if(this.getVisible() === false) {
+                this.getDom().hide();
+            }
+            //render content-items
+            this._renderContent();
+        }
+    },
+
+    _renderContent: function() {
+        var aObjects = this.getContent();
+        if(typeof aObjects == 'object' && aObjects) {
+            var dom = this.getDom();
+            if(aObjects.length > 0) {
+                var t = this;
+                Josie.utils.each(aObjects, function(oObject) {
+                    t._renderContentItem(oObject);
+                });
+            } else {
+                this._renderContentItem(aObjects);
+            }
+        }
+    },
+
+    _renderContentItem: function(oItem) {
+        this.getDom().append(oItem.getDom());
+        oItem.invalidate();
+    },
+
+    _forceUpdateProperties: function() {
+        var aProperties = this.getProperties();
+        jQuery.each(aProperties, jQuery.proxy(function(key, value) {
+            var setter = 'set'+Josie.utils.capitalize(key);
+            if(this[setter]) {
+                Josie.log.trace('Call '+setter, value);
+                this[setter].call(this, value);
+            }
+        }, this));
+    },
+
+    rerender: function(bWithoutUpdateProperties) {
+        this.trigger('onBeforeRenderer');
+        this.replaceDom('<div />').remove();
+        this._renderControl();
+        if(!bWithoutUpdateProperties)
+            this._forceUpdateProperties();
+        this.trigger('onAfterRenderer');
+    },
+
+    invalidate: function() {
+        Josie.log.debug('Invalidating '+this.className);
+        if(this.__valid !== false) {
+            this.__valid = false;
+            setTimeout(jQuery.proxy(function() {
+                this.__valid = true;
+                this.rerender();
+            }, this), 0);
+        }
+    },
 
     replaceDom: function(sContent) {
         var jqOldDom = this.getDom(),
@@ -113,5 +123,5 @@ com.nysoft.josie.core.ManagedObject.extend('com.nysoft.josie.core.Control', {
         this.setCssClasses([]);  // empty it for next write or writing-step
         return ' class="' + aCssClasses.join(' ') + '" ';
     }
-	
+
 });
